7a092e13ad2b3760199e4cdda10aab2f4c23d0c5
PC <lucaslorran2016@gmail.com>
2026-04-08 11:51:46 -0300

    Adiciona pasta screenmatch

---
+++ screenmatch/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Marverick");
+
+        int anoDeLancamento = 2022;
+        boolean incluiNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+    }
+}
